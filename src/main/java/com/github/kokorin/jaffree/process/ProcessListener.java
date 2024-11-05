d3ae1cc497ab89caf16c1061c44cf5d2e51f990d
Speiger <speiger@gmx.net>
2024-11-05 14:03:52 +0100

    Updated Process Listener for things that made sense to do.

--- src/main/java/com/github/kokorin/jaffree/process/ProcessListener.java
+++ src/main/java/com/github/kokorin/jaffree/process/ProcessListener.java
@@ -8,8 +8,17 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author Speiger
  */
 public interface ProcessListener {
-	public void onStart(Process process);
-	public void onStop(Process process);
+	/**
+	 * Provides the Process instance that was started
+	 * @param process the process
+	 */
+	void onStart(Process process);
+	
+	/**
+	 * Provides the Process instance that was just completed
+	 * @param process the process
+	 */
+	void onStop(Process process);
 	
 	/**
 	 * Simple tracker wrapper that allows to track all instances being loaded.
@@ -17,7 +26,7 @@ public interface ProcessListener {
 	 * @param instances Set. Highly Suggest {@link Collections#newSetFromMap} using a {@link ConcurrentHashMap} for multithreading support
 	 * @return ProcessListener wrapper
 	 */
-	public static ProcessListener of(Set<Process> instances) {
+	static ProcessListener of(Set<Process> instances) {
 		return new Impl(instances);
 	}
 	
@@ -27,12 +36,12 @@ public interface ProcessListener {
 		public Impl(Set<Process> instances) {
 			this.instances = instances;
 		}
-
+		
 		@Override
 		public void onStart(Process process) {
 			instances.add(process);
 		}
-
+		
 		@Override
 		public void onStop(Process process) {
 			instances.remove(process);
